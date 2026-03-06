ddd4682fa724524fbfa3bfa98d1cf552157e05f7
jakariya0dev <jhalhadi@gmail.com>
2026-03-06 23:59:20 +0600

    ui: navbar responsive done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,21 +29,22 @@ export default function () {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a>Item 1</a>
+                <a>Home</a>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <a>FAQ</a>
               </li>
               <li>
-                <a>Item 3</a>
+                <a>Changelog</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+              <li>
+                <a>Download</a>
+              </li>
+              <li>
+                <a>Contact</a>
               </li>
             </ul>
           </div>
